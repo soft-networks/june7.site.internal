d6d763834ea966d3e660cd7fa25774b3e1ca0fbb
Neel Rao <raoneel@gmail.com>
2025-06-07 11:14:57 -0700

    fix api key

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const OPEN_ROUTER_API_KEY = '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';
+const OPEN_ROUTER_API_KEY = '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';
 
 export async function POST(request: NextRequest) {
     const { message } = await request.json();
@@ -12,7 +12,7 @@ export async function POST(request: NextRequest) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'openai/gpt-4o',
+        model: 'google/gemini-2.0-flash-001',
         messages: [
           {
             role: 'user',
