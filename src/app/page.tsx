7fff8adde353aea39fc79675d3287a2e3bc77b19
bhavik singh <bhavik@bhaviks-WorkBook-2.local>
2025-06-07 13:12:27 -0400

    add landscape vibes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Happiness } from "@/components/hapiness";
+import { LandscapeBackground } from "./landscapbackground";
 
 export default function Home() {
   return (
     <div>
       <div style={{position: "absolute", bottom: "10%", left: "10%" }}>
         <Happiness />
+        <LandscapeBackground />
       </div>
     </div>
   );
