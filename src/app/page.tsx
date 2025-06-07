58f30c416bc5731308fcb5680e5df80600b55d8e
Neel Rao <raoneel@gmail.com>
2025-06-07 10:43:48 -0700

    tom cruise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import styles from "./page.module.css";
 import { Happiness } from "@/components/hapiness";
 import { LandscapeBackground } from "../components/landscapbackground";
 import { Grimaldis } from "@/components/grimaldis";
+import { TomCruise } from "@/components/tomcruise";
 
 export default function Home() {
   return (
@@ -15,6 +16,9 @@ export default function Home() {
       </div>
       <div style={{position: "absolute", bottom: "10%", right: "10%" }}>
         <Grimaldis />
+      </div>
+      <div style={{position: "absolute", bottom: "30%", right: "20%" }}>
+        <TomCruise />
       </div>
     </div>
   );
---
+++ src/components/tomcruise.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export function TomCruise() {
+    return (
+        <div style={{
+            animation: 'rotate 20s linear infinite',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }}>
+            <Image src='/cruise.png' alt='Tom Cruise' width={200} height={200} />
+            <div>I do my own stunts! Like this one!</div>
+            <style jsx>{`
+                @keyframes rotate {
+                    from {
+                        transform: rotate(0deg);
+                    }
+                    to {
+                        transform: rotate(360deg);
+                    }
+                }
+            `}</style>
+        </div>
+    )
+}
