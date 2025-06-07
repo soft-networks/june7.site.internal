545541a3369c21840bc655488bd8ab3de7d9c9e4
Neel Rao <raoneel@gmail.com>
2025-06-07 10:07:49 -0700

    add grimaldis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Happiness } from "@/components/hapiness";
+import { Grimaldis } from "@/components/grimaldis";
 
 export default function Home() {
   return (
     <div>
       <div style={{position: "absolute", bottom: "10%", left: "10%" }}>
         <Happiness />
+      </div>
+      <div style={{position: "absolute", bottom: "10%", right: "10%" }}>
+        <Grimaldis />
       </div>
     </div>
   );
